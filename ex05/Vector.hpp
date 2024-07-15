3f9f2839be760b3a24713f347e20ed761435e489
Sheree Morphett <sheree.morphett@gmail.com>
2024-07-15 15:00:26 +0300

    ex05 tests written

---
+++ ex05/Vector.hpp
@@ -0,0 +1,184 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <limits>
+#include <cmath>
+
+template <typename T>
+struct Vector
+{
+    std::vector<T> components;
+
+    Vector(const std::vector<T>& v) : components(v) {};
+    Vector() {}
+
+    // Constructor to initialize from individual components
+    Vector(std::initializer_list<T> init) : components(init) {}
+
+    size_t size() const
+    {
+        return components.size();
+    }
+
+    T& operator[](size_t index)
+    {
+        return components[index];
+    }
+
+    const T& operator[](size_t index) const
+    {
+        return components[index];
+    }
+
+    Vector operator+(const Vector& rhs) const
+    {
+        if (size() != rhs.size())
+            throw std::invalid_argument("Vector addition requires vectors of the same size.");
+
+        Vector result;
+        result.components.reserve(size());
+        for (size_t i = 0; i < size(); ++i)
+        {
+            result.components.push_back(components[i] + rhs.components[i]);
+        }
+        return result;
+    }
+    
+    Vector operator-(const Vector& rhs) const
+    {
+        if (size() != rhs.size())
+            throw std::invalid_argument("Vector addition requires vectors of the same size.");
+    
+        Vector result;
+        result.components.reserve(size());
+        for (size_t i = 0; i < size(); ++i)
+        {
+            result.components.push_back(components[i] - rhs.components[i]);
+        }
+
+        return result;
+    }
+
+    Vector operator*(const T scalar) const
+    {
+        Vector result;
+        result.components.reserve(size());
+        for (size_t i = 0; i < size(); ++i)
+        {
+            result.components.push_back(components[i] * scalar);
+        }
+
+        return result;
+    }
+    
+    
+    T dot(Vector const & b)
+    {
+        if (components.size() != b.size())
+        {
+            throw std::invalid_argument("Vectors must be of the same size");
+        }
+
+        T result;
+
+        for (size_t i = 0; i < b.size(); ++i)
+        {
+            result += components[i] * b[i];
+        }
+
+        return result;
+    }
+
+
+    T norm_one()
+    {
+        T result = 0;
+
+        for (auto i : components)
+        {
+            result += std::abs(i);
+        }
+
+        return result;
+    }
+
+
+    T norm()
+    {
+        T result = 0;
+
+        for (auto i : components)
+        {
+            result += i * i;
+        }
+
+        return std::sqrt(result);
+
+    }
+
+    T norm_inf()
+    {
+
+        if (components.empty())
+        {
+            return 0.0;
+        }
+
+        T max_val = std::abs(components[0]);
+
+        for (const auto& el : components)
+        {
+            if (std::abs(el) > max_val)
+                max_val = std::abs(el);
+        }
+
+        return max_val;
+    }
+
+};
+
+
+template <typename T>
+T lerp_single(T const & u, T const & v, float t)
+{
+    return u + t * (v - u);
+}
+
+template <typename VecType>
+VecType lerp( VecType const & u, VecType  const & v, float t) {
+    if (u.size() != v.size()) {
+        throw std::invalid_argument("Vectors must be of the same size");
+    }
+
+    VecType result;
+    for (size_t i = 0; i < u.size(); ++i) {
+        result[i] = lerp_single(u[i], v[i], t);
+    }
+
+    return result;
+}
+
+
+template <typename T>
+T angle_cos(Vector<T> const & u, Vector<T> const & v)
+{
+
+}
+
+
+
+template <typename T>
+std::ostream& operator<<(std::ostream & os, Vector<T> const & vec)
+{
+    os << "[ ";
+    for (size_t i = 0; i < vec.size(); ++i) {
+        os << vec[i];
+        if (i != vec.size() - 1) {
+            os << ", ";
+        }
+    }
+    os << " ]";
+    return os;
+}
---
+++ ex05/ex05.cpp
@@ -0,0 +1,54 @@
+#include "amv_math.hpp"
+#include "colours.hpp"
+#include <vector>
+#include "Vector.hpp"
+
+
+#include <stdexcept>
+#include <iostream>
+
+/*
+
+
+*/
+
+
+
+
+int main() {
+
+    Vector<float> u = {1.0, 0.0};
+    Vector<float> v = {0.0, 1.0};
+
+    std::cout << FCYN("Expected Output: ") << std::endl;
+    std::cout << FCYN("1.0") << std::endl;
+    std::cout << angle_cos(u, v);
+
+    u = {-1.0, 1.0};
+    v = {1.0, 1.0};
+    std::cout << FCYN("Expected Output: ") << std::endl;
+    std::cout << FCYN("0.0") << std::endl;
+    std::cout << angle_cos(u, v);
+
+    u = {-1.0, 1.0};
+    v = {1.0, -1.0};
+    std::cout << FCYN("Expected Output: ") << std::endl;
+    std::cout << FCYN("-1.0") << std::endl;
+    std::cout << angle_cos(u, v);
+
+
+    u = {2.0, 1.0};
+    v = {4.0, 2.0};
+    std::cout << FCYN("Expected Output: ") << std::endl;
+    std::cout << FCYN("1.0") << std::endl;
+    std::cout << angle_cos(u, v);
+
+
+    u = {1.0, 2.0, 3.0};
+    v = {4.0, 5.0, 6.0};
+    std::cout << FCYN("Expected Output: ") << std::endl;
+    std::cout << FCYN("0.974631846") << std::endl;
+    std::cout << angle_cos(u, v);
+
+    return 0;
+}
